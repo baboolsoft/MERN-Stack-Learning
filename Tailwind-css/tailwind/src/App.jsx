ac851617f929d20628fcf49ee576f0cb335a69c2
SuyaBaboolsoft2002 <suryarameshkumar2002@gmail.com>
2025-09-12 16:28:49 +0530

    Added postcss

--- Tailwind-css/tailwind/src/App.jsx
+++ Tailwind-css/tailwind/src/App.jsx
@@ -6,19 +6,19 @@ export default function App() {
           Tailwind CSS Basics 
         </h1>
         <p className="text-gray-700">
-          React + Vite + Tailwind v4 setup with best practices.
+          React + Vite + Tailwind v4.
         </p>
       </header>
 
       <main className="mt-8 space-y-4">
-        <button className="px-4 py-2 bg-brand-500 text-white rounded-lg shadow hover:bg-brand-700 focus:ring-2 focus:ring-offset-2 focus:ring-brand-500">
-          Primary Button
+        <button className="px-4 py-2 bg-brand-500 text-black rounded-lg shadow hover:bg-brand-700 focus:ring-2 focus:ring-offset-2 focus:ring-brand-500">
+          Hi, Everyone
         </button>
 
         <div className="p-4 border border-gray-200 rounded-md bg-white shadow">
           <h2 className="font-semibold mb-2">Card Component</h2>
           <p className="text-sm text-gray-600">
-            This is a simple card example styled with Tailwind utilities.
+            This is a Card.
           </p>
         </div>
       </main>
